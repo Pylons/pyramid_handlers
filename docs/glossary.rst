7385a72608f44fc50ba970acd2a9d58c0b3a1880
Chris McDonough <chrism@plope.com>
2011-01-16 15:15:17 -0500

    first cut

---
+++ docs/glossary.rst
@@ -0,0 +1,21 @@
+.. _glossary:
+
+Glossary
+========
+
+.. glossary::
+   :sorted:
+
+   Pyramid
+      A `web framework <http://pylonshq.com/pyramid>`_.
+
+   View handler
+     A view handler ties together
+     :meth:`pyramid.config.Configurator.add_route` and
+     :meth:`pyramid.config.Configurator.add_view` to make it more
+     convenient to register a collection of views as a single class when
+     using :term:`url dispatch`.  See also :ref:`views_chapter`.
+
+   Pylons
+     `A lightweight Python web framework <http://pylonshq.com>`_.
+
---
+++ setup.py
@@ -0,0 +1,65 @@
+##############################################################################
+#
+# Copyright (c) 2008-2010 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+__version__ = '0.0'
+
+import os
+import platform
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires=[
+    'pyramid',
+    ]
+
+if platform.system() == 'Java':
+    tests_require = install_requires
+else:
+    tests_require= install_requires + ['Sphinx', 'docutils', 
+                                       'repoze.sphinx.autointerface']
+
+setup(name='pyramid_handlers',
+      version=__version__,
+      description='Experimental handler support for Pyramid',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "License :: Repoze Public License",
+        ],
+      keywords='web wsgi pylons pyramid',
+      author="Chris McDonough, Agendaless Consulting",
+      author_email="pylons-devel@googlegroups.com",
+      url="http://docs.pylonsproject.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = install_requires,
+      tests_require = tests_require,
+      test_suite="pyramid_handlers",
+      entry_points = """
+      """
+      )
+
